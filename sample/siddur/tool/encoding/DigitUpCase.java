3bc6fd00c610f3ba09105709efd766c1438e9d3d
siddur <htm1840324@163.com>
2013-12-24 11:15:34 +0800

    a

--- sample/siddur/tool/encoding/DigitUpCase.java
+++ sample/siddur/tool/encoding/DigitUpCase.java
@@ -15,28 +15,47 @@ public class DigitUpCase implements ITool {
 	@Override
 	public String[] execute(String[] inputs, IToolWrapper toolWrapper, Map<String, Object> context) throws Exception {
 		String digitText = inputs[0];
-		long digit = Long.parseLong(digitText);
+
+		//validate
+		if(digitText.startsWith("-")){
+			return new String[]{"无效的输入"};
+		}
+		if(digitText.contains(".")){
+			return new String[]{"无效的输入"};
+		}
+		long digit = 0;
+		try {
+			digit = Long.parseLong(digitText);
+		} catch (Exception e) {
+			return new String[]{"无效的输入"};
+		}
 		StringBuilder sb = new StringBuilder(20);
 		int i = 0;
-		while(digit > 0){
-			i++;
+		do{
 			long newDigit = digit/10;
 			int mod = (int)(digit - newDigit*10);
 			transfer(sb, mod, i);
+			i++;
 			digit = newDigit;
-		}
+		}while (digit > 0);
 		
 		//去掉拾前面的壹
-		if(sb.charAt(sb.length() - 1) == '壹' && sb.charAt(sb.length() - 2) == '拾'){
+		if(sb.length() >= 2 && sb.charAt(sb.length() - 1) == '壹' && sb.charAt(sb.length() - 2) == '拾'){
 			sb.deleteCharAt(sb.length() - 1);
 		}
 		
-		return new String[]{sb.reverse().toString()};
+		//去掉末尾的零
+		if(sb.length() > 1 && sb.charAt(0) == '零'){
+			sb.deleteCharAt(0);
+		}
+		
+		String result = sb.reverse().toString();
+		return new String[]{result};
 	}
 	
 	
 	
-	private void transfer(StringBuilder sb, int mod, int i){
+	private void transfer(StringBuilder sb, int mod, int x){
 		boolean repeat0 = false;
 		if(mod == 0){
 			repeat0 = found0;
@@ -45,7 +64,6 @@ public class DigitUpCase implements ITool {
 			found0 = false;
 		}
 		
-		int x = i - 1;
 		int loc = x%4;
 		if(x > 0){
 			if(loc == 0){
@@ -59,7 +77,7 @@ public class DigitUpCase implements ITool {
 					sb.append(A[mod]);
 			}
 		}
-		else if(!found0){
+		else{
 			sb.append(A[mod]);
 		}
 	}
@@ -78,10 +96,22 @@ public class DigitUpCase implements ITool {
 
 	public static void main(String[] args) throws Exception {
 		DigitUpCase d = new DigitUpCase();
-		String[] inputs = new String[]{"120078"};
+		String[] inputs = new String[]{"0"};
 		System.out.print(inputs[0]);
 		System.out.println(d.execute(inputs, null, null)[0]);
 		inputs = new String[]{"123456789"};
+		System.out.print(inputs[0]);
+		System.out.println(d.execute(inputs, null, null)[0]);
+		inputs = new String[]{"10"};
+		System.out.print(inputs[0]);
+		System.out.println(d.execute(inputs, null, null)[0]);
+		inputs = new String[]{"100"};
+		System.out.print(inputs[0]);
+		System.out.println(d.execute(inputs, null, null)[0]);
+		inputs = new String[]{"101"};
+		System.out.print(inputs[0]);
+		System.out.println(d.execute(inputs, null, null)[0]);
+		inputs = new String[]{"1011000"};
 		System.out.print(inputs[0]);
 		System.out.println(d.execute(inputs, null, null)[0]);
 	}
